f998a975551982996bb9bca545023ea5447b61e7
invite-you <50567167+invite-you@users.noreply.github.com>
2020-10-17 01:25:16 +0900

    Update custom_aug.py

--- mmdet/datasets/custom_aug.py
+++ mmdet/datasets/custom_aug.py
@@ -30,7 +30,7 @@ class CustomAugmentation(object):
             x1, y1, x2, y2 = box
             bboxes.append( ia.BoundingBox(x1=x1, y1=y1, x2=x2, y2=y2) )        
         
-        image_aug, bbs_aug, segmaps_aug = self.seq(images=[image], bounding_boxes=[bboxes], segmentation_maps=[masks])
+        image_aug, bbs_aug, segmaps_aug = self.seq(images=[img], bounding_boxes=[bboxes], segmentation_maps=[masks])
         image_aug, bbs_aug, segmaps_aug = image_aug[0], bbs_aug[0], segmaps_aug=[0]
         print(image_aug.shape, bbs_aug.shape, segmaps_aug.shape)
         
