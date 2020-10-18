8225fe9d4a24b9cfdc7ef9bab4df34519bbcf2a3
invite-you <50567167+invite-you@users.noreply.github.com>
2020-10-18 23:54:12 +0900

    Update htc_without_semantic_r50_fpn_1x_dota1_5.py

--- configs/DOTA1_5/htc_without_semantic_r50_fpn_1x_dota1_5.py
+++ configs/DOTA1_5/htc_without_semantic_r50_fpn_1x_dota1_5.py
@@ -1,4 +1,16 @@
 # model settings
+#    pretrained='open-mmlab://resnext101_64x4d',
+#    interleaved=True,
+#    mask_info_flow=True,
+#    backbone=dict(
+#        type='ResNeXt',
+#        depth=101,
+#        groups=64,
+#        base_width=4,
+#        num_stages=4,
+#        out_indices=(0, 1, 2, 3),
+#        frozen_stages=1,
+#        style='pytorch'),  
 model = dict(
     type='HybridTaskCascade',
     num_stages=3,
