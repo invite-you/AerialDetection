70da0f3c99571afe470a63d7e5ba4a1bd5b36fdf
invite-you <50567167+invite-you@users.noreply.github.com>
2020-10-18 23:49:41 +0900

    Update cascade_mask_rcnn_r50_fpn_1x_dota1_5.py

--- configs/DOTA1_5/cascade_mask_rcnn_r50_fpn_1x_dota1_5.py
+++ configs/DOTA1_5/cascade_mask_rcnn_r50_fpn_1x_dota1_5.py
@@ -1,4 +1,14 @@
 # model settings
+#    pretrained='open-mmlab://resnext101_32x4d',    
+#    backbone=dict(
+#        type='ResNeXt',
+#        depth=101,
+#        groups=32,
+#        base_width=4,
+#        num_stages=4,
+#        out_indices=(0, 1, 2, 3),
+#        frozen_stages=1,
+#        style='pytorch'),
 model = dict(
     type='CascadeRCNN',
     num_stages=3,
