7b73aa96daa41efa869d696b95841d98442e480b
invite-you <50567167+invite-you@users.noreply.github.com>
2020-10-25 21:08:44 +0900

    Update faster_rcnn_RoITrans_r50_fpn_1x_dota1_5.py

--- configs/DOTA1_5/faster_rcnn_RoITrans_r50_fpn_1x_dota1_5.py
+++ configs/DOTA1_5/faster_rcnn_RoITrans_r50_fpn_1x_dota1_5.py
@@ -18,7 +18,7 @@ model = dict(
         type='RPNHead',
         in_channels=256,
         feat_channels=256,
-        anchor_scales=[8],
+        anchor_scales=[4],
         anchor_ratios=[0.5, 1.0, 2.0],
         anchor_strides=[4, 8, 16, 32, 64],
         target_means=[.0, .0, .0, .0],
@@ -37,7 +37,7 @@ model = dict(
         in_channels=256,
         fc_out_channels=1024,
         roi_feat_size=7,
-        num_classes=17,
+        num_classes=5,
         target_means=[0., 0., 0., 0., 0.],
         target_stds=[0.1, 0.1, 0.2, 0.2, 0.1],
         reg_class_agnostic=True,
@@ -56,7 +56,7 @@ model = dict(
         in_channels=256,
         fc_out_channels=1024,
         roi_feat_size=7,
-        num_classes=17,
+        num_classes=5,
         target_means=[0., 0., 0., 0., 0.],
         target_stds=[0.05, 0.05, 0.1, 0.1, 0.05],
         reg_class_agnostic=False,
@@ -125,40 +125,42 @@ test_cfg = dict(
     rpn=dict(
         # TODO: test nms 2000
         nms_across_levels=False,
-        nms_pre=2000,
-        nms_post=2000,
-        max_num=2000,
+        nms_pre=3000,
+        nms_post=3000,
+        max_num=3000,
         nms_thr=0.7,
         min_bbox_size=0),
     rcnn=dict(
         # score_thr=0.05, nms=dict(type='py_cpu_nms_poly_fast', iou_thr=0.1), max_per_img=1000)
-        score_thr = 0.05, nms = dict(type='py_cpu_nms_poly_fast', iou_thr=0.1), max_per_img = 2000)
+        score_thr = 0.05, nms = dict(type='py_cpu_nms_poly_fast', iou_thr=0.1), max_per_img = 3000)
 # soft-nms is also supported for rcnn testing
     # e.g., nms=dict(type='soft_nms', iou_thr=0.5, min_score=0.05)
 )
 # dataset settings
-dataset_type = 'DOTA1_5Dataset_v2'
-data_root = 'data/dota1_1024/'
+dataset_type = 'DOTA1_5Dataset_v2_car'
+data_root = '/content/gdrive/My Drive/Arirang/data/train/custom_coco_all_car/'
+imgae_root = '/content/gdrive/My Drive/Arirang/data/train/coco_all/'
 img_norm_cfg = dict(
-    mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
+    mean=[54.06, 53.295, 50.235], std=[36.72, 35.955, 33.915], to_rgb=True)
 data = dict(
     imgs_per_gpu=2,
     workers_per_gpu=2,
     train=dict(
         type=dataset_type,
         ann_file=data_root + 'trainval1024/DOTA1_5_trainval1024.json',
-        img_prefix=data_root + 'trainval1024/images/',
+        img_prefix=imgae_root + 'trainval1024/images/',
         img_scale=(1024, 1024),
         img_norm_cfg=img_norm_cfg,
         size_divisor=32,
         flip_ratio=0.5,
         with_mask=True,
         with_crowd=True,
-        with_label=True),
+        with_label=True,
+        custom_aug=True),
     val=dict(
         type=dataset_type,
         ann_file=data_root + 'trainval1024/DOTA1_5_trainval1024.json',
-        img_prefix=data_root + 'trainval1024/images/',
+        img_prefix=imgae_root + 'trainval1024/images/',
         img_scale=(1024, 1024),
         img_norm_cfg=img_norm_cfg,
         size_divisor=32,
@@ -168,8 +170,8 @@ data = dict(
         with_label=True),
     test=dict(
         type=dataset_type,
-        ann_file=data_root + 'test1024/DOTA1_5_test1024.json',
-        img_prefix=data_root + 'test1024/images',
+        ann_file= '/content/gdrive/My Drive/Arirang/data/test/instances_test2017.json',
+        img_prefix= '/content/gdrive/My Drive/Arirang/data/test/images/',
         # ann_file=data_root + 'test1024_ms/DOTA_test1024_ms.json',
         # img_prefix=data_root + 'test1024_ms/images',
         img_scale=(1024, 1024),
@@ -189,7 +191,7 @@ lr_config = dict(
     warmup_iters=500,
     warmup_ratio=1.0 / 3,
     step=[8, 11])
-checkpoint_config = dict(interval=12)
+checkpoint_config = dict(interval=5)
 # yapf:disable
 log_config = dict(
     interval=50,
@@ -199,10 +201,10 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 12
+total_epochs = 1200
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
-work_dir = './work_dirs/faster_rcnn_RoITrans_r50_fpn_1x_dota1_5'
-load_from = None
+work_dir = './work_dirs/faster_rcnn_RoITrans_r50_fpn_1x_dota1_5_car'
+load_from = '/content/gdrive/My Drive/Arirang/epoch_12.pth'
 resume_from = None
 workflow = [('train', 1)]
