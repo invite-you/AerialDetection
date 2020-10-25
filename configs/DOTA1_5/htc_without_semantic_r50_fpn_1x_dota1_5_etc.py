68a1e1c75f2834e7f7b9790fc8227bee00c13156
invite-you <50567167+invite-you@users.noreply.github.com>
2020-10-25 18:05:46 +0900

    Update htc_without_semantic_r50_fpn_1x_dota1_5_etc.py

--- configs/DOTA1_5/htc_without_semantic_r50_fpn_1x_dota1_5_etc.py
+++ configs/DOTA1_5/htc_without_semantic_r50_fpn_1x_dota1_5_etc.py
@@ -260,7 +260,7 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 6000
+total_epochs = 2000
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = '/content/gdrive/My Drive/Arirang/models/htc_without_semantic_r50_fpn_1x_dota1_5_etc/'
